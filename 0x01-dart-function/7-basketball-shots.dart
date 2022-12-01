603c3ae4b9f837dd587d916d53512d81f72fa4a1
ecastan960 <ecastan1@eafit.edu.co>
2022-11-30 20:44:50 -0500

    [01] Task 7

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -6,9 +6,9 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   numbA2 = numbA2 * 2;
   numbA3 = numbA3 * 3;
   var numbA = numbA1 + numbA2 + numbA3;
-  var numbB1 = teamB['Free throws'] ?? -1;
-  var numbB2 = teamB['2 pointer'] ?? -1;
-  var numbB3 = teamB['3 pointer'] ?? -1;
+  var numbB1 = teamB['Free throws'] ?? 0;
+  var numbB2 = teamB['2 pointer'] ?? 0;
+  var numbB3 = teamB['3 pointer'] ?? 0;
   numbB1 = numbB1 * 1;
   numbB2 = numbB2 * 2;
   numbB3 = numbB3 * 3;
