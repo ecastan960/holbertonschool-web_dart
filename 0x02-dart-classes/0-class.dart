3c59adc25c6a011abd7380fb86a6e14860575a42
ecastan960 <ecastan1@eafit.edu.co>
2022-12-01 22:27:41 -0500

    [00] Task 0

---
+++ 0x02-dart-classes/0-class.dart
@@ -0,0 +1,9 @@
+class User {  
+   // field 
+   String name = "";  
+   
+   // function 
+   String showName() { 
+      return('Hello ${name}'); 
+   } 
+}
--- 0x02-dart-classes/README.md
+++ 0x02-dart-classes/README.md
@@ -1,1 +1,1 @@
-
+# 0x02. Dart - Classes
