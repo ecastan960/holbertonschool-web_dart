bec324e63b2b6f88891460d5513a3faa07f35d03
ecastan960 <ecastan1@eafit.edu.co>
2022-12-02 21:02:24 -0500

    [00] Task 3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,36 @@
+class Password {
+  String _password;
+  Password({required String password}) : _password = password;
+  String get password => _password;
+
+  bool isValid() {
+    if ((password.length < 8) | (password.length > 16)) {
+      return false;
+    }
+    bool checkUppercase = false;
+    bool checkLowercase = false;
+    bool checkNumber = false;
+    for (int i = 0; i < password.length; i++) {
+      if (password[i] == password[i].toUpperCase()) {
+        checkUppercase = true;
+      }
+      if (password[i] == password[i].toLowerCase()) {
+        checkLowercase = true;
+      }
+      if (double.tryParse(password[i]) != null) {
+        checkNumber = true;
+      }
+    }
+    if ((checkUppercase == false) |
+        (checkLowercase == false) |
+        (checkNumber == false)) {
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: ${this.password}';
+  }
+}
