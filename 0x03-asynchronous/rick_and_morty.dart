f0ea1b02fccd18b9fc523f75c319b709b533e543
ecastan960 <ecastan1@eafit.edu.co>
2022-12-06 23:18:12 -0500

    [03] Task 5

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -8,7 +8,7 @@ printRmCharacters() async {
     );
     var body = json.decode(res.body);
 
-    for (var i = 0; i < 17; i++) {
+    for (var i = 0; i < body['results'].length; i++) {
       print("${body['results'][i]['name']}");
     }
   } catch (err) {
