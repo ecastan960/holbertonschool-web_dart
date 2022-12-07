55f512d34d6beb8efcf5d4cd8b775bd92cb2518c
ecastan960 <ecastan1@eafit.edu.co>
2022-12-06 22:41:31 -0500

    [00] Task 4

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,21 @@
+import '4-util.dart';
+import 'dart:convert';
+
+Future<double> calculateTotal() async {
+  try {
+    var res = await fetchUserData();
+    Map user = json.decode(res);
+    var id = user['id'];
+    var product = await fetchUserOrders(id);
+    double sum = 0;
+    var value = '';
+    var products = json.decode(product);
+    for (var i = 0; i < products.length; i++) {
+      value = await fetchProductPrice(products[i]);
+      sum += double.parse(value);
+    }
+    return sum;
+  } catch (err) {
+    return -1;
+  }
+}
